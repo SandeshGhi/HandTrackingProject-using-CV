f698a15689c5218693ce3739580a6ddb39e43495
Jhapendra <86385889+einsteinjhaps@users.noreply.github.com>
2021-10-01 09:24:06 +0545

    sliding movement added

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -60,6 +60,24 @@ class handDetector():
             else:
                 fingers.append(0)
         return fingers
+    
+    #sliding fingers
+    def fingersSlide(self):
+        fingers = []
+
+        # Right Slide
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Left Slide
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
 
 
 def main():
